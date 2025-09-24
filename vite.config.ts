3ab7111b67eb06c14ea3bcba8e0c471e2fef5165
lolshaji <ayahakuttyv@gmail.com>
2025-09-25 00:43:56 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
- HEAD
-  const env = loadEnv(mode, '.', '');
-  return {
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    },
-    base: "/thelden/",  // 👈 ADD THIS LINE (replace with your GitHub repo name)
-  };
-
     const env = loadEnv(mode, '.', '');
     return {
       server: {
@@ -40,5 +20,4 @@ export default defineConfig(({ mode }) => {
         }
       }
     };
- c4e4d0c2847f8cc6b14cd9847f0c5ca78f240263
 });
